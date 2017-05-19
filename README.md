0e925d42e5aa3b39b12e675fe83987fc0a029cff
marcedem <marcsea2000@gmail.com>
2017-05-19 17:16:37 +0200

    First step setting up the webpack config

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # webpackWorldProject
 Playing with webpack configuration and setups for various aspect of development for a real world project
+
+This project is based on React JS.
--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,21 @@ var webpack = require('webpack');
 var path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
-  }
+    entry: './src/index.js',
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+                use: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/ // Do not include any file from node_modules since they are already in ES5 format.
+            },
+            {
+                use: ['style-loader', 'css-loader'],
+                test: /\.css$/
+            }
+        ]
+    }
 };
