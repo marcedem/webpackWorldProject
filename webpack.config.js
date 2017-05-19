7e0396da2e7135e0dfd753ae5ba59a43d1715780
marcedem <marcsea2000@gmail.com>
2017-05-19 18:14:21 +0200

    Code splitting and cache management with webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,20 @@
 var webpack = require('webpack');
 var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+// List of dependencies that are less frequently updated
+const VENDOR_LIBS = [
+    'faker', 'lodash', 'redux', 'react-redux', 'react-dom', 'react-input-range', 'redux-form', 'redux-thunk', 'react'
+];
 
 module.exports = {
-    entry: './src/index.js',
+    entry: {
+        bundle: './src/index.js', // application JS files 
+        vendor: VENDOR_LIBS // depencencies JS files
+    },
     output: {
         path: path.join(__dirname, 'dist'),
-        filename: 'bundle.js'
+        filename: '[name].[chunkhash].js' // hash the content and put it out as a hash name to avoid caching issues
     },
     module: {
         rules: [{
@@ -18,5 +27,14 @@ module.exports = {
                 test: /\.css$/
             }
         ]
-    }
+    },
+    plugins: [
+        // avoid duplicating dependencies in both bundle and vendor js
+        new webpack.optimize.CommonsChunkPlugin({
+            names: ['vendor', 'manifest']
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        })
+    ]
 };
